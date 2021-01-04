716afe684bd64ef19180c457464ed2fcd45520cc
Rohit <rohitcricbuzz@gmail.com>
2021-01-04 21:36:48 +0000

    Assert equal function

---
+++ assertEqual.js
@@ -0,0 +1,12 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("💚💚💚Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("😡😡😡Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual("Bootcamp", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(2, 3);
