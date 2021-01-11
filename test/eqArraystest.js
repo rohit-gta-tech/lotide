113c6d0ece5d02485989039bf309606855c95d10
Rohit <rohitcricbuzz@gmail.com>
2021-01-11 23:45:48 +0000

    eqArrays refactored and test cases incorporated m/c

--- test/eqArraystest.js
+++ test/eqArraystest.js
@@ -1,17 +1,31 @@
+const assert = require('chai').assert
 const eqArrays = require('../eqArrays');
-const assertEqual = require('../assertEqual')
+
+describe("#eqArrays", function() {
+    it("return [1,2,3] for [1,2,3]", function() {
+        assert.isTrue(eqArrays([1, 2, 3], [1, 2, 3]));
+    });
+    it("return [] for []", function() {
+        assert.isTrue(eqArrays([], []));
+    });
+    it("return [1,2,3] for [1,2,3,4]", function() {
+        assert.isFalse(eqArrays([1, 2, 3], [1, 2, 3, 4]));
+    });
+    it("return [1,2,3] for [3,2,1]", function() {
+        assert.isFalse(eqArrays([1, 2, 3], [3,2,1]));
+    });
+    it("return [1,2,3] for [1,2,'3']", function() {
+        assert.isFalse(eqArrays([1, 2, 3], [1, 2, "3"]));
+    });
+    it("return [1, 2, [1,[1,2,3,4,[1,2,3,[1,2,3]]]]] for [1, 2, [1,[1,2,3,4,[1,2,3,[1,2,3]]]]]", function() {
+        assert.isTrue(eqArrays([1, 2, [1,[1,2,3,4,[1,2,3,[1,2,3]]]]], [1, 2, [1,[1,2,3,4,[1,2,3,[1,2,3]]]]]));
+    });
+    it("return [1, 2, [1,[1,2,3,4,[1,2,3,[1,2,4]]]]] for [1, 2, [1,[1,2,3,4,[1,2,3,[1,2,3]]]]]", function() {
+        assert.isFalse(eqArrays([1, 2, [1,[1,2,3,4,[1,2,3,[1,2,4]]]]], [1, 2, [1,[1,2,3,4,[1,2,3,[1,2,3]]]]]));
+    });
 
 
-eqArrays([1, 2, 3], [1, 2, 3]) 
-eqArrays([1, 2, 3], [1, 2, 3, 4]) 
-eqArrays([1, 2, 3], [3, 2, 1]) 
-
-eqArrays(["1", "2", "3"], ["1", "2", "3"]) 
-eqArrays(["1", "2", "3"], ["1", "2", 3]) 
+});
 
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); 
-assertEqual(eqArrays([1, 2, "3"], [1, 2, 3]), false); 
 
-assertEqual(eqArrays([1, 2, [1,[1,2,3,4,[1,2,3,[1,2,3]]]]], [1, 2, [1,[1,2,3,4,[1,2,3,[1,2,3]]]]]), true); 
-assertEqual(eqArrays([1, 2, [1,[1,2,3,4,[1,2,3,[1,2,4]]]]], [1, 2, [1,[1,2,3,4,[1,2,3,[1,2,3]]]]]), false); 
